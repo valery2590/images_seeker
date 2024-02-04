06e2c65a8c9cebefdf0a3713f91cff86afe49f9d
valery2590 <valeryfigueroah@gmail.com>
2024-02-04 00:53:40 +0000

    new changes

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -4,32 +4,32 @@ import "./Image.css";
 const Image = ({ src, alt, href, key, tags, likes }) => {
   return (
     <div className="image__container" key={key}>
-        
-        <a
-            href={href}
-            className="aReference_container"
-            target="_blank"
-            rel="noreferrer"
-                >
-            <img className="image__component"
-                src={src} 
-                alt={alt} 
-                key={key}/>
-            <div className="text_image_component">
-                    <p className="tag_text">
-                    <span role="img" aria-label="tags" className="emoji">
-                        🏷️</span>
-                    {tags}
-                    </p>
-                    <p className="like_text">
-                    <span role="img" aria-label="likes" className="emoji">
-                    👍</span> 
-                    {likes}
-                    </p>
-            </div>
-        </a>
+      <a
+        href={href}
+        className="aReference_container"
+        target="_blank"
+        rel="noreferrer"
+      >
+      <img className="image__component" src={src} alt={alt} key={key} />
+      </a>
+      <div className="text_image_component">
+          <p className="tag_text">
+            <span role="img" aria-label="tags" className="emoji">
+              🏷️
+            </span>
+            {tags}
+          </p>
+          <p className="like_text">
+            <span role="img" aria-label="likes" className="emoji">
+              👍
+            </span>
+            {likes}
+          </p>
+        </div>
+       
+
     </div>
-);
+  );
 };
 
 export default Image;
--- src/components/List/ImageList.js
+++ src/components/List/ImageList.js
@@ -1,100 +1,99 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios'
-import ImageInput from '../Input/ImageInput';
-import ImageButton from '../Button/ImageButton';
-import Image from '../Image/Image'
-import "./ImageList.css"
-
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import ImageInput from "../Input/ImageInput";
+import ImageButton from "../Button/ImageButton";
+import Image from "../Image/Image";
+import "./ImageList.css";
 
 
 const ImageList = () => {
-
-const [image, setImages] = useState([])
-const [termino, setTermino]= useState( 
-  ()=>{
+  const [image, setImages] = useState([]);
+  const [termino, setTermino] = useState(() => {
     const saved = localStorage.getItem("local");
     const initialValue = JSON.parse(saved);
     return initialValue || "";
-  }
-)
+  });
 
-const url = "https://pixabay.com/api/"
-const API_KEY = '19774551-55ee3622621fff95a0958e798'
+  const url = "https://pixabay.com/api/";
+  const API_KEY = "19774551-55ee3622621fff95a0958e798";
 
-// API 
-const getData = async ()=>{
-  const res = await axios.get(`${url}?key=${API_KEY}&q=${termino}&image_type=photo&type=photo&per_page=100`)
-  setImages(res.data.hits)
-}
+  // API
+  const getData = async () => {
+    try {
+      const res = await axios.get(
+        `${url}?key=${API_KEY}&q=${termino}&image_type=photo&type=photo&per_page=50`
+      );
+      console.log(res, "info de api");
+      const allImages = res.data.hits
+      setImages(allImages);
+      
+    } catch (error) {
+      console.log(error)
+    }
+  };
 
-useEffect(()=>{ 
-    getData()
-},[])// eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getData();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-const consultarApi = ()=>{
-  axios.get(`${url}?key=${API_KEY}&q=${termino}&image_type=photo&per_page=100`)
-    .then(response=>{
-      setImages(response.data.hits)
-    })
+  const consultarApi = async () => {
+    axios
+      .get(`${url}?key=${API_KEY}&q=${termino}&image_type=photo&per_page=100`)
+      .then((response) => {
+         setImages(response.data.hits);
+      });
     // setTermino('')
-}
-//LOCAL STORAGE
+  };
+  //LOCAL STORAGE
 
-useEffect(()=>{
-  const data =   localStorage.getItem("local")
-  setTermino(JSON.parse(data))
-  },[])
+  useEffect(() => {
+    const data = localStorage.getItem("local");
+    setTermino(JSON.parse(data));
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("local", JSON.stringify(termino));
+  }, [termino]);
 
-useEffect(()=>{
-    localStorage.setItem("local",JSON.stringify(termino))
-},[termino])
+  // KEY ENTER EVENT
+  const listener = (event) => {
+    if (event.code === "Enter" || event.code === "NumpadEnter") {
+      event.preventDefault();
+      consultarApi();
+    }
+  };
 
-
-
-
-
-// KEY ENTER EVENT  
-const listener = event => {
-  if (event.code === "Enter" || event.code === "NumpadEnter") {
-    event.preventDefault();
-    consultarApi();
-  }
-};
-
-
-    return (
-      <div className='imageList_container'> 
-
-      <div className='first_section_imageList'>
-      <ImageInput 
-              onChange={(e)=>(setTermino(e.target.value))}
-              value={termino}
-              onKeyPress={listener}/>
-        <ImageButton onClick={consultarApi}/>  
+  return (
+    <div className="imageList_container">
+      <div className="first_section_imageList">
+        <ImageInput
+          onChange={(e) => setTermino(e.target.value)}
+          value={termino}
+          onKeyPress={listener}
+        />
+        <ImageButton onClick={consultarApi} />
       </div>
-        
-        <div className='second_section_imageList'>
-          {image.length >= 1 &&
-          image.map ((user, idx)=>
-          <Image 
-            key={user.largeImageURL}
-            href={user.largeImageURL} 
-            src={user.largeImageURL} 
-                  alt={user.name} 
-                  tags={user.tags}
-                  likes={user.likes}
-          />
-            )}
 
+      <div className="second_section_imageList">
+        {image.length >= 1 &&
+          image.map((user, idx) => (
+            <Image
+              key={user.largeImageURL}
+              href={user.largeImageURL}
+              src={user.largeImageURL}
+              alt={user.name}
+              tags={user.tags.split(',')[0]+ ','+ user.tags.split(',')[1]}
+              likes={user.likes}
+            />
+          ))}
+      </div>
+      {image.length === 0 && (
+        <div className="noResult_section_container">
+          <p className="noResult_text">No hay ps pendejo!</p>
         </div>
-        {image.length === 0 &&  
-              <div className='noResult_section_container'>
-                <p className='noResult_text'>No hay ps pendejo!</p>
-              </div>
-           }
-        
-      </div>
-    )}
+      )}
+    </div>
+  );
+};
 
 export default ImageList;
